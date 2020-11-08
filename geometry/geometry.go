abed33b9bce86c7cc4e63e563b9be6d9b5284c21
Sheen An <sheen.an.goh@gmail.com>
2020-11-08 21:39:41 +0800

    Added error handling

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -1,5 +1,9 @@
 package geometry
 
-func Cube(n int) int {
-	return n * n * n
+func Cube(n int) (int, err) {
+	if n! = 0 {
+		return n * n * n, nil
+	} else {
+		return 0, errors.New("Zero length edge is not allowed")
+	}
 }
