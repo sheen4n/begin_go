37beca44201564a9ff8c926ca1a000a7f1d25626
Sheen An <sheen.an.goh@gmail.com>
2020-11-08 21:43:42 +0800

    Fixed ! bug

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -1,7 +1,9 @@
 package geometry
 
+import "errors"
+
 func Cube(n int) (int, err) {
-	if n! = 0 {
+	if n != 0 {
 		return n * n * n, nil
 	} else {
 		return 0, errors.New("Zero length edge is not allowed")
